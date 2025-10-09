8d6aa426579f04bcab8cd64d44c14e587b4def8d
dockerdaemon <volkoofsenya@gmail.com>
2025-10-09 11:53:27 +0300

    fix: классы названия

--- lab4/calc.js
+++ lab4/calc.js
@@ -1,9 +1,9 @@
-const num1 = document.querySelector('.calc__input--num1');
-const num2 = document.querySelector('.calc__input--num2');
-const operation = document.querySelector('.calc__select--operation');
-const output = document.querySelector('.calc__output');
+const num1 = document.querySelector('.inputs__input-form--num1');
+const num2 = document.querySelector('.inputs__input-form--num2');
+const operation = document.querySelector('.inputs__select--operation');
+const output = document.querySelector('.result__output');
 const button = document.querySelector('.calc__button');
-const historyList = document.querySelector('.calc-history__list');
+const historyList = document.querySelector('.history__list');
 
 if (!num1 || !num2 || !operation || !output || !button || !historyList) {
   console.error('Interface Error☠ ');
@@ -11,15 +11,12 @@ if (!num1 || !num2 || !operation || !output || !button || !historyList) {
   button.addEventListener('click', () => {
     const a = parseFloat(num1.value);
     const b = parseFloat(num2.value);
-
     if (isNaN(a) || isNaN(b)) {
       output.textContent = "Error: input must be a number!";
       return;
     }
-
     let result;
     let opSymbol;
-
     switch (operation.value) {
       case 'add':
         result = a + b;
@@ -44,18 +41,14 @@ if (!num1 || !num2 || !operation || !output || !button || !historyList) {
       default:
         result = 0;
     }
-
     result = parseFloat(result.toFixed(2));
     // вывод результата
     output.textContent = result;
-
     // создаём элемент истории
     const item = document.createElement('div');
-    item.className = 'calc-history__item';
+    item.className = 'history__item';
     item.textContent = `${a} ${opSymbol} ${b} = ${result}`;
-
     // добавляем в историю
     historyList.appendChild(item);
   });
 }
-
