25700b61e73f4fdb8305332eff3845a1305a752d
Pavel-Gornov <gornovpavel30@yandex.ru>
2025-10-20 12:45:58 +0400

    Полная реализация задумки

--- lab4/script.js
+++ lab4/script.js
@@ -1,12 +1,16 @@
 const ACHIEVEMENTS_STRINGS = {
-    "1": ["Начало Арифметики", "Как дважды два!"],
-    "2": ["Остепенение", "Возведите число в квадрат"], // Открывает оператор степени
-    "3": ["Корень зла", "Извлеките корень из 666"],
-    "4": ["Полный беспредел!", "Достигните бесконечности"],
-    "5": ["Великая константа", "Аппроксимируйте число π"],
-    "6": ["Замечательная константа", "Аппроксимируйте число ⅇ"],
-    "7": ["Золотая константа", "Аппроксимируйте число φ"]
+    "2x2": ["Начало Арифметики", "Как дважды два!"],
+    "pow": ["Остепенение", "Возведите число в квадрат"], // Открывает оператор степени
+    "666": ["Корень зла", "Извлеките корень из 666"],
+    "inf": ["Полный беспредел!", "Достигните бесконечности"],
+    "nan": ["Что-то сломалось...", "Получите NaN"],
+    "i": ["Мнимый успех", "Попытайтесь получить мнимое число"],
+    "0": ["Не по правилам", "Разделите на 0"],
+    "pi": ["Великая константа", "Аппроксимируйте число π"],
+    "e": ["Замечательная константа", "Аппроксимируйте число ⅇ"], // Открывает логарифмирование
+    "phi": ["Золотая константа", "Аппроксимируйте число φ"]
 }
+const PHI = 1.61803398874
 
 let ACHIEVEMENTS = {}
 
@@ -27,6 +31,53 @@ function init() {
     create_toast("Добро пожаловать!");
 }
 
+function achievements(a, b, op, res) {
+    /* Это такой себе код, но и я не JS разработчик */
+    if (a === 2 && b === 2 && (op === "*" || op === "+") && !ACHIEVEMENTS["2x2"]) {
+        ACHIEVEMENTS["2x2"] = true;
+        create_toast(ACHIEVEMENTS_STRINGS["2x2"]);
+    }
+    if (a === b && op === "*" && !ACHIEVEMENTS["pow"]) {
+        ACHIEVEMENTS["pow"] = true;
+        create_toast(ACHIEVEMENTS_STRINGS["pow"]);
+        const l1 = document.getElementById("locked1");
+        l1.style.display = "block";
+    }
+    if (!isFinite(res) && !ACHIEVEMENTS["inf"]) {
+        ACHIEVEMENTS["inf"] = true;
+        create_toast(ACHIEVEMENTS_STRINGS["inf"]);
+    }
+    if (isNaN(res) && !ACHIEVEMENTS["nan"]) {
+        ACHIEVEMENTS["nan"] = true;
+        create_toast(ACHIEVEMENTS_STRINGS["nan"]);
+    }
+    if (a === 666 && b === (1/2) && op === "^" && !ACHIEVEMENTS["666"]) {
+        ACHIEVEMENTS["666"] == true;
+        create_toast(ACHIEVEMENTS_STRINGS["666"]);
+    }
+    if (Math.abs(res - Math.PI) < 0.001 && !ACHIEVEMENTS["pi"]) {
+        ACHIEVEMENTS["pi"] = true;
+        create_toast(ACHIEVEMENTS_STRINGS["pi"]);
+    }
+    if (Math.abs(res - Math.E) < 0.001 && !ACHIEVEMENTS["e"]) {
+        ACHIEVEMENTS["e"] = true;
+        create_toast(ACHIEVEMENTS_STRINGS["e"]);
+        const l2 = document.getElementById("locked2");
+        l2.style.display = "block";
+    }
+    if (Math.abs(res - PHI) < 0.001 && !ACHIEVEMENTS["phi"]) {
+        ACHIEVEMENTS["phi"] = true;
+        create_toast(ACHIEVEMENTS_STRINGS["phi"]);
+    }
+    if (a < 0 && b === (1/2) && op === "^" && !ACHIEVEMENTS["i"]) {
+        ACHIEVEMENTS["i"] = true;
+        create_toast(ACHIEVEMENTS_STRINGS["i"]);
+    }
+    if ( b === 0 && op === "/" && !ACHIEVEMENTS["0"]) {
+        ACHIEVEMENTS["0"] = true;
+        create_toast(ACHIEVEMENTS_STRINGS["0"]);
+    }
+}
 
 function calculate() {
     const a = document.getElementById("operand1");
@@ -65,14 +116,24 @@ function calculate() {
         default:
             console.error("Ошибка оператора.");
     }
-
     result.textContent = `Результат: ${res}`
+    achievements(num_a, num_b, operator.value, res);
 }
 
 function create_toast(text) {
     const toast = document.createElement("div");
     toast.className = "toast-list__achievement-toast";
-    toast.textContent = text;
+    if (Array.isArray(text)) {
+        for (let string of text) {
+            const p = document.createElement("p")
+            p.textContent = string;
+            p.className = "achievement-toast__string";
+            toast.appendChild(p);
+        }
+    }
+    else {
+        toast.textContent = text;
+    }
     toast.addEventListener("animationend", function() {toast.remove();});
     const list = document.getElementById("toast-list");
     list.appendChild(toast);
