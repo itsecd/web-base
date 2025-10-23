d6667c287e6172ab9803a9b410e073f28d761eef
Pavel-Gornov <gornovpavel30@yandex.ru>
2025-10-23 22:14:41 +0400

    Реструктуризация скрипта

--- lab4/script.js
+++ lab4/script.js
@@ -1,20 +1,38 @@
 const ACHIEVEMENTS_STRINGS = {
-    "2x2": ["Начало Арифметики", "Как дважды два!"],
-    "pow": ["Остепенение", "Возведите число в квадрат"], // Открывает оператор степени
-    "666": ["Корень зла", "Извлеките корень из 666"],
-    "inf": ["Полный беспредел!", "Достигните бесконечности"],
-    "nan": ["Что-то сломалось...", "Получите NaN"],
-    "i": ["Мнимый успех", "Попытайтесь получить мнимое число"],
-    "0": ["Не по правилам", "Разделите на 0"],
-    "pi": ["Великая константа", "Аппроксимируйте число π"],
-    "e": ["Замечательная константа", "Аппроксимируйте число ⅇ"], // Открывает логарифмирование
-    "phi": ["Золотая константа", "Аппроксимируйте число φ"]
+    "2x2": { "text": ["Начало Арифметики", "Как дважды два!"], "active": false, 
+        "condition": (a, b, op, res) => (a === 2 && b === 2 && (op === "*" || op === "+")) },
+    "pow": { "text": ["Остепенение", "Возведите число в квадрат"], "active": false,
+        "condition": (a, b, op, res) => (a === b && op === "*"),
+        "unlock": () => { const l1 = document.getElementById("locked1"); l1.style.display = "block"; }
+    }, // Открывает оператор степени
+    "666": { "text":["Корень зла", "Извлеките корень из 666"], "active": false,
+        "condition": (a, b, op, res) => (a === 666 && b === (1/2) && op === "^")
+    },
+    "inf": { "text":["Полный беспредел!", "Достигните бесконечности"], "active": false,
+        "condition": (a, b, op, res) => (!isFinite(res) && !isNaN(res))
+    },
+    "nan": { "text":["Что-то сломалось...", "Получите NaN"], "active": false,
+        "condition": (a, b, op, res) => (isNaN(res))
+    },
+    "i": { "text":["Мнимый успех", "Попытайтесь получить мнимое число"], "active": false,
+        "condition": (a, b, op, res) => (a < 0 && Math.abs(b) === (1/2) && op === "^")
+    },
+    "0": { "text":["Не по правилам", "Разделите на 0"], "active": false,
+        "condition": (a, b, op, res) => (b === 0 && op === "/")
+    },
+    "pi": { "text":["Великая константа", "Аппроксимируйте число π"], "active": false,
+        "condition": (a, b, op, res) => (Math.abs(res - Math.PI) < 0.001)
+    },
+    "e": {  "text":["Замечательная константа", "Аппроксимируйте число ⅇ"], "active": false,
+        "condition": (a, b, op, res) => (Math.abs(res - Math.E) < 0.001),
+        "unlock": () => {const l2 = document.getElementById("locked2"); l2.style.display = "block";}
+    }, // Открывает логарифмирование
+    "phi": { "text":["Золотая константа", "Аппроксимируйте число φ"], "active": false,
+        "condition": (a, b, op, res) => (Math.abs(res - PHI) < 0.001)
+    }
 }
+
 const PHI = 1.61803398874
-
-let ACHIEVEMENTS = {}
-
-window.onload = init;
 
 function log_b(a, b) {
     return Math.log(a) / Math.log(b);
@@ -22,6 +40,7 @@ function log_b(a, b) {
 
 
 // . . .
+window.onload = init;
 function init() {
     const l1 = document.getElementById("locked1");
     if (l1) {l1.style.display = "none";} else { console.error("Не удалось получить все элементы."); }
@@ -32,51 +51,14 @@ function init() {
 }
 
 function achievements(a, b, op, res) {
-    /* Это такой себе код, но и я не JS разработчик */
-    if (a === 2 && b === 2 && (op === "*" || op === "+") && !ACHIEVEMENTS["2x2"]) {
-        ACHIEVEMENTS["2x2"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["2x2"]);
-    }
-    if (a === b && op === "*" && !ACHIEVEMENTS["pow"]) {
-        ACHIEVEMENTS["pow"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["pow"]);
-        const l1 = document.getElementById("locked1");
-        l1.style.display = "block";
-    }
-    if (!isFinite(res) && !isNaN(res) && !ACHIEVEMENTS["inf"]) {
-        ACHIEVEMENTS["inf"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["inf"]);
-    }
-    if (isNaN(res) && !ACHIEVEMENTS["nan"]) {
-        ACHIEVEMENTS["nan"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["nan"]);
-    }
-    if (a === 666 && b === (1/2) && op === "^" && !ACHIEVEMENTS["666"]) {
-        ACHIEVEMENTS["666"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["666"]);
-    }
-    if (Math.abs(res - Math.PI) < 0.001 && !ACHIEVEMENTS["pi"]) {
-        ACHIEVEMENTS["pi"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["pi"]);
-    }
-    if (Math.abs(res - Math.E) < 0.001 && !ACHIEVEMENTS["e"]) {
-        ACHIEVEMENTS["e"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["e"]);
-        const l2 = document.getElementById("locked2");
-        l2.style.display = "block";
-    }
-    if (Math.abs(res - PHI) < 0.001 && !ACHIEVEMENTS["phi"]) {
-        ACHIEVEMENTS["phi"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["phi"]);
-    }
-    if (a < 0 && Math.abs(b) === (1/2) && op === "^" && !ACHIEVEMENTS["i"]) {
-        ACHIEVEMENTS["i"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["i"]);
-    }
-    if ( b === 0 && op === "/" && !ACHIEVEMENTS["0"]) {
-        ACHIEVEMENTS["0"] = true;
-        create_toast(ACHIEVEMENTS_STRINGS["0"]);
-    }
+    /* Выглядит куда опрятнее, чем кучка if-выражений */
+    Object.values(ACHIEVEMENTS_STRINGS).forEach( (achievement) => {
+        if (!achievement["active"] && achievement["condition"](a, b, op, res)) {
+            create_toast(achievement["text"]);
+            achievement["active"] = true;
+            if (achievement["unlock"]) achievement["unlock"]();
+        }
+    });
 }
 
 function calculate() {
