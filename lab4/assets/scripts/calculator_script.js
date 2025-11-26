e6595e5815415dfbe3702df994279dd818890695
clayw <clayworks11337@gmail.com>
2025-11-26 20:22:20 +0400

    Реализован функционал калькулятора в JS

---
+++ lab4/assets/scripts/calculator_script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const inputs = document.querySelectorAll(".top-wrapper__input");
+    const select = document.querySelector(".top-wrapper__selection");
+    const button = document.querySelector(".bottom-wrapper__result");
+    const output = document.querySelector(".central-wrapper__output");
+
+    button.addEventListener("click", () => {
+        const num1 = parseFloat(inputs[0].value);
+        const num2 = parseFloat(inputs[1].value);
+        const op = select.value;
+
+        if (isNaN(num1) || isNaN(num2)) {
+            output.textContent = "Введите оба числа!";
+            return;
+        }
+        if (op === "/" && num2 === 0) {
+            output.textContent = "На ноль делить нельзя!";
+            return;
+        }
+
+        let result;
+        switch (op) {
+            case "+": 
+                result = num1 + num2;
+                break;
+            case "-": 
+                result = num1 - num2; 
+                break;
+            case "*": 
+                result = num1 * num2; 
+                break;
+            case "/": 
+                result = num1 / num2; 
+                break;
+        }
+
+        output.textContent = "Результат: " + result;
+    });
+});
