6bdcf313321d6175d3d872d0fc3246785cab0234
Дмитрий Левин <d.lvin999@mail.ru>
2025-12-08 12:06:58 +0400

    Исправил

---
+++ lab4_js.js
@@ -0,0 +1,43 @@
+function calculator() {
+  const num1Input = document.getElementById('num1');
+  const num2Input = document.getElementById('num2');
+  const operatorSelect = document.getElementById('operator');
+  const resultDiv = document.getElementById('result');
+
+  if (!num1Input || !num2Input || !operatorSelect || !resultDiv) {
+    console.error('Один или несколько необходимых элементов не найдены!');
+    return;
+  }
+
+  const num1 = parseFloat(num1Input.value);
+  const num2 = parseFloat(num2Input.value);
+  const op = operatorSelect.value;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    resultDiv.textContent = 'Введите числа';
+    return;
+  }
+
+  if (op === '/' && num2 === 0) {
+    resultDiv.textContent = 'Ошибка: Зачем делить на 0...';
+    return;
+  }
+
+  let result;
+  switch (op) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      result = num1 / num2;
+      break;
+  }
+
+  resultDiv.textContent = 'Результат: ' + result;
+}
