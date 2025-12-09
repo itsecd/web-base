a45a9c52ee41fbd3ab2e3d781cf291f88c213fd7
microalbina <shalimova.albina07@gmail.com>
2025-12-09 20:44:10 +0400

    Добавлена проверка того, что в поля ввели числа; в querySelector к названиям классов добавлены теги; основная функция теперь выполняется, когда загрузилась страница

--- lab4_var22/script.js
+++ lab4_var22/script.js
@@ -51,21 +51,30 @@ function calculate({a, b, operation}) {
     return result;
 };
 
-const inputANode = document.querySelector('.calculator__first-number');
-const inputBNode = document.querySelector('.calculator__second-number');
-const selectOperationsNode = document.querySelector('.calculator__select-operations');
-const calculateButtonNode = document.querySelector('.calculator__calculate-button');
-const resultNode = document.querySelector('.calculator__result');
+const calculateButtonNode = document.querySelector('button.calculator__calculate-button');
+const resultNode = document.querySelector('p.calculator__result');
 
-calculateButtonNode.addEventListener('click', function() {
-    const a = Number(inputANode.value);
-    const b = Number(inputBNode.value);
-    const operation = selectOperationsNode.value;
+document.addEventListener("DOMContentLoaded", () => {
+    calculateButtonNode.addEventListener('click', function() {
 
-    try {
-        const result = calculate({a, b, operation});
-        resultNode.innerHTML = result;
-    } catch (error) {
-        resultNode.innerHTML = `<span>Ошибка: ${error.message}</span>`;
-    }
+        const inputANode = document.querySelector('input.calculator__first-number');
+        const inputBNode = document.querySelector('input.calculator__second-number');
+        const selectOperationsNode = document.querySelector('select.calculator__select-operations');
+
+        if ( inputANode.value === "" || inputBNode.value === "") {
+            resultNode.innerHTML = `<span>Ошибка: Оба поля должны быть заполнены</span>`
+            return;
+        }
+
+        const a = Number(inputANode.value);
+        const b = Number(inputBNode.value);
+        const operation = selectOperationsNode.value;
+
+        try {
+            const result = calculate({a, b, operation});
+            resultNode.innerHTML = result;
+        } catch (error) {
+            resultNode.innerHTML = `<span>Ошибка: ${error.message}</span>`;
+        }
+    })
 });
