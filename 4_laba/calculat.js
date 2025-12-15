179329a19ab1e2841d1cf2ed3419edef452d9daf
BlaZeriTUwU <schumakownikita@gmail.com>
2025-12-15 17:50:30 +0400

    Добавлены изменения

--- 4_laba/calculat.js
+++ 4_laba/calculat.js
@@ -1,55 +1,56 @@
+function updateValue(result) {
+    display.textContent = result;
+}
+
+function calculate() {
+    let result;
+    const num1 = parseFloat(number1.value);
+    const num2 = parseFloat(number2.value);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        alert("Введите оба числа!");
+        updateValue('-');
+        return;
+    }
+
+    switch(operatorSelect.value) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                alert("Ошибка: деление на ноль!");
+                return;
+            } else {
+                result = num1 / num2;
+            }
+            break;
+        default:
+            return;
+    }
+
+    updateValue(result.toFixed(2));
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM загружен');
 
-    const display = document.getElementById('result');
-    const number1 = document.getElementById('num1');
-    const number2 = document.getElementById('num2');
-    const operatorSelect = document.getElementById('operation');
-    const calculateButton = document.getElementById('calculate');
+    display = document.getElementById('result');
+    number1 = document.getElementById('num1');
+    number2 = document.getElementById('num2');
+    operatorSelect = document.getElementById('operation');
+    calculateButton = document.getElementById('calculate');
 
     if (!display || !number1 || !number2 || !operatorSelect || !calculateButton) {
         console.error('Ошибка: не найдены элементы HTML');
         return;
-    }
-
-    function updateValue(result) {
-        display.textContent = result;
-    }
-
-    function calculate() {
-        let result;
-        const num1 = parseFloat(number1.value);
-        const num2 = parseFloat(number2.value);
-
-        if (isNaN(num1) || isNaN(num2)) {
-            alert("Введите оба числа!");
-            updateValue('-');
-            return;
-        }
-
-        switch(operatorSelect.value) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 === 0) {
-                    alert("Ошибка: деление на ноль!");
-                    return;
-                } else {
-                    result = num1 / num2;
-                }
-                break;
-            default:
-                return;
-        }
-
-        updateValue(result.toFixed(2));
     }
 
     calculateButton.addEventListener('click', calculate);
