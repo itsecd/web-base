fa6d86746230baea52a316734745c3c319abbe10
meow <rinegeto@gmail.com>
2025-12-15 20:57:21 +0400

    Update calculator.js

--- lab4/calculator.js
+++ lab4/calculator.js
@@ -1,41 +1,48 @@
 function calculate() {
-  const num1 = document.getElementById("num1").value;
-  const num2 = document.getElementById("num2").value;
-  const operation = document.getElementById("operation").value;
-  const resultField = document.getElementById("result");
-  const errorField = document.getElementById("error");
 
-  errorField.textContent = "";
-  resultField.textContent = "Результат: —";
+  try {
 
-  if (num1 === "" || num2 === "") {
-    errorField.textContent = "Пожалуйста, введите оба числа 💔";
-    return;
+    const num1 = document.getElementById("num1").value;
+    const num2 = document.getElementById("num2").value;
+    const operation = document.getElementById("operation").value;
+    const resultField = document.getElementById("result");
+    const errorField = document.getElementById("error");
+
+    errorField.textContent = "";
+    resultField.textContent = "Результат: —";
+
+    if (num1 === "" || num2 === "") {
+      errorField.textContent = "Пожалуйста, введите оба числа 💔";
+      return;
+    }
+
+    const a = Number(num1);
+    const b = Number(num2);
+    let result;
+
+    if (operation === "/" && b === 0) {
+      errorField.textContent = "На ноль делить нельзя 😾";
+      return;
+    }
+
+    switch (operation) {
+      case "+":
+        result = a + b;
+        break;
+      case "-":
+        result = a - b;
+        break;
+      case "*":
+        result = a * b;
+        break;
+      case "/":
+        result = a / b;
+        break;
+    }
+
+    resultField.textContent = `Результат: ${result}`;
+
+  } catch (error) {
+    console.error("Ошибка калькулятора:", error);
   }
-
-  const a = Number(num1);
-  const b = Number(num2);
-  let result;
-
-  if (operation === "/" && b === 0) {
-    errorField.textContent = "На ноль делить нельзя 😾";
-    return;
-  }
-
-  switch (operation) {
-    case "+":
-      result = a + b;
-      break;
-    case "-":
-      result = a - b;
-      break;
-    case "*":
-      result = a * b;
-      break;
-    case "/":
-      result = a / b;
-      break;
-  }
-
-  resultField.textContent = `Результат: ${result}`;
 }
