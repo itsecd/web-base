80e5aa41817a8cadeb3c58b6d325293b9cf22c90
xe-n0x <bakhtinucheba860@gmail.com>
2025-12-15 23:35:53 +0400

    updated lab4
    Исправил замечание

--- lab4/scripts.js
+++ lab4/scripts.js
@@ -1,21 +1,24 @@
 function calculate(){
     const el1 = document.getElementById('n1');
     const el2 = document.getElementById('n2');
-    const opr = document.getElementById('operations')
+    const opr = document.getElementById('operations');
+    const resultEl = document.getElementById('result');
+    const errorEl = document.getElementById('error');
 
-    if (!el1 || !el2 || !opr){
-        console.error('Какой-либо элемент не найден\n')
+   if (!el1 || !el2 || !opr || !resultEl || !errorEl) {
+        console.error('Один или несколько элементов не найдены')
+        return;
     }
 
     const num1 = el1.value.trim();
     const num2 = el2.value.trim();
     const operation = opr.value;
 
-    document.getElementById('result').textContent = '';
-    document.getElementById('error').textContent = '';
+    resultEl.textContent = '';
+    errorEl.textContent = '';
 
     if (num1 === '' || num2 === ''){
-        document.getElementById('error').textContent = 'Поля должны быть заполнены';
+        errorEl.textContent = 'Поля должны быть заполнены';
         return;
     }
 
@@ -23,7 +26,7 @@ function calculate(){
     const b = Number(num2);
 
     if (isNaN(a) || isNaN(b)){
-        document.getElementById('error').textContent = 'Введите корректные числа.';
+        errorEl.textContent = 'Введите корректные числа.';
         return;
     }
 
@@ -40,17 +43,16 @@ function calculate(){
             break;
         case "divide":
             if (b === 0){
-                document.getElementById('error').textContent = 'Деление на 0 невозможно';
+                errorEl.textContent = 'Деление на 0 невозможно';
                 return;
             }
             result = a/b;
             break;
         default:
-            document.getElementById('error').textContent = 'Неизвестная операция, попробуйте снова'
+            errorEl.textContent = 'Неизвестная операция, попробуйте снова'
             return;
     }
-
-    document.getElementById('result').textContent = `Результат: ${result}`;
+    resultEl.textContent = `Результат: ${result}`;
 }
 
 document.getElementById('button').addEventListener('click', calculate);
